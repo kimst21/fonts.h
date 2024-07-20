7728f566a2761ccf68b3ce710dfe19eaa3f82399
kimst21 <118581137+kimst21@users.noreply.github.com>
2024-07-20 18:21:41 +0900

    Update fonts.h

--- fonts.h
+++ fonts.h
@@ -1,24 +1,3 @@
-/**
- * original author:  Tilen Majerle<tilen@majerle.eu>
- * modification for STM32f10x: Alexander Lutsai<s.lyra@ya.ru>
-   ----------------------------------------------------------------------
-    Copyright (C) Alexander Lutsai, 2016
-    Copyright (C) Tilen Majerle, 2015
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-   ----------------------------------------------------------------------
- */
 #ifndef FONTS_H
 #define FONTS_H 120
 
@@ -51,17 +30,17 @@ extern C {
  * @brief  Font structure used on my LCD libraries
  */
 typedef struct {
-  uint8_t FontWidth;    /*!< Font width in pixels */
-  uint8_t FontHeight;   /*!< Font height in pixels */
-  const uint16_t *data; /*!< Pointer to data font data array */
+	uint8_t FontWidth;    /*!< Font width in pixels */
+	uint8_t FontHeight;   /*!< Font height in pixels */
+	const uint16_t *data; /*!< Pointer to data font data array */
 } FontDef_t;
 
 /**
  * @brief  String length and height
  */
 typedef struct {
-  uint16_t Length;      /*!< String length in units of pixels */
-  uint16_t Height;      /*!< String height in units of pixels */
+	uint16_t Length;      /*!< String length in units of pixels */
+	uint16_t Height;      /*!< String height in units of pixels */
 } FONTS_SIZE_t;
 
 /**
